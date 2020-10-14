81309ee73e22e3c1f57b8e7fd9f1452d77d9911f
Matias Blazquez <matiasblazquez@gmail.com>
2020-10-14 01:28:34 -0500

    Message script

--- scripts/sendMessage.sh
+++ scripts/sendMessage.sh
@@ -1,21 +1,31 @@
 #/bin/bash
 
-if [ $TRAVIS_BRANCH == "DEVOP-222" ]; then
-    GIT=`which git`
+# this script clones a repository, including all its remote branches
+# Author: julianromera
 
-    if [ "x$1" = "x" -o "x$2" = "x" ];then
-    echo "use: $0 <git_repository_to_clone> <directory>"
-    exit 1
-    fi
+GIT=`which git`
 
-    if [ "x$GIT" = "x" ];then
-    echo "No git command found. install it"
-    fi
-
-    function clone {
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'Un mensaje mas completo'}" https://hooks.slack.com/services/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
-    }
-
-    clone
+if [ "x$1" = "x" -o "x$2" = "x" ];then
+  echo "use: $0 <git_repository_to_clone> <directory>"
+  exit 1
 fi
 
+if [ "x$GIT" = "x" ];then
+  echo "No git command found. install it"
+fi
+
+function clone {
+
+  message=$GIT log master..DEVOP-222 --pretty=format:"%h%x09%an%x09%ad%x09%s"  --merges
+
+  echo "Im inside $2 now I can run the script"
+
+  curl -X POST -H 'Content-type: application/json' --data "{'text':'${message}'}" https://hooks.slack.com/services/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
+  
+  # for remote in `$GIT branch -r | grep -v \>`; do
+  #    $GIT branch --track ${remote#origin/} $remote;
+  # done
+}
+
+echo "cloning repository into ... $2"
+clone $1 $2
