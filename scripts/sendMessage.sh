a60a063900c2652eb33fe09a1307c0041ec3411b
Matias Blazquez <matiasblazquez@gmail.com>
2020-10-14 01:43:45 -0500

    Message script

--- scripts/sendMessage.sh
+++ scripts/sendMessage.sh
@@ -2,7 +2,7 @@
 
 function message {
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
 }
 
 message
