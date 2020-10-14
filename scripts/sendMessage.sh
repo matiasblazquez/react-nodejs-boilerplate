634ba8a681fd8d2e0886348011ae34533a5113b2
Matias Blazquez <matiasblazquez@gmail.com>
2020-10-14 01:14:16 -0500

    Message script

--- scripts/sendMessage.sh
+++ scripts/sendMessage.sh
@@ -13,19 +13,9 @@ if [ "$TRAVIS_BRANCH" == "DEVOP-222" ]; then
     fi
 
     function clone {
-
-    message=$GIT log master..DEVOP-1364 --pretty=format:"%h%x09%an%x09%ad%x09%s"  --merges
-
-    echo "Im inside $2 now I can run the script"
-
-    curl -X POST -H 'Content-type: application/json' --data "{'text':'${message}'}" https://hooks.slack.com/services/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
-    
-    # for remote in `$GIT branch -r | grep -v \>`; do
-    #    $GIT branch --track ${remote#origin/} $remote;
-    # done
+    curl -X POST -H 'Content-type: application/json' --data "{'text':'Un mensaje mas completo'}" https://hooks.slack.com/services/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
     }
 
-    echo "cloning repository into ... $2"
-    clone $1 $2
+    clone
 fi
 
