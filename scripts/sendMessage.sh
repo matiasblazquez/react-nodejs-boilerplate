aae16ccb95dd4ad4ccdd19bebf7d37f057cd374f
Matias Blazquez <matiasblazquez@gmail.com>
2020-10-14 01:30:41 -0500

    Message script

--- scripts/sendMessage.sh
+++ scripts/sendMessage.sh
@@ -5,11 +5,6 @@
 
 GIT=`which git`
 
-if [ "x$1" = "x" -o "x$2" = "x" ];then
-  echo "use: $0 <git_repository_to_clone> <directory>"
-  exit 1
-fi
-
 if [ "x$GIT" = "x" ];then
   echo "No git command found. install it"
 fi
@@ -18,8 +13,6 @@ function clone {
 
   message=$GIT log master..DEVOP-222 --pretty=format:"%h%x09%an%x09%ad%x09%s"  --merges
 
-  echo "Im inside $2 now I can run the script"
-
   curl -X POST -H 'Content-type: application/json' --data "{'text':'${message}'}" https://hooks.slack.com/services/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
   
   # for remote in `$GIT branch -r | grep -v \>`; do
@@ -27,5 +20,4 @@ function clone {
   # done
 }
 
-echo "cloning repository into ... $2"
-clone $1 $2
+clone
